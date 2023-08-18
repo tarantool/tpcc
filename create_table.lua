b582e94dd49b2b5d361d2889e8ee12bc3797bcbb
Mikhail Elhimov <m.elhimov@vk.team>
2023-08-18 14:21:36 +0300

    support both box.sql.execute and box.execute
    Part of #8 (fix step 3 from the Quickstart guide)

--- create_table.lua
+++ create_table.lua
@@ -1,8 +1,13 @@
 box.cfg{listen = 3301, memtx_memory = 10 * 1024^3}
 box.schema.user.grant('guest', 'read,write,execute', 'universe')
 
-box.sql.execute("drop table if exists warehouse;")
-box.sql.execute("create table warehouse ( \
+-- Prior to 2.2.1 box.sql.execute was used for SQL queries.
+if not box.execute then
+    box.execute = box.sql.execute
+end
+
+box.execute("drop table if exists warehouse;")
+box.execute("create table warehouse ( \
 w_id int not null, \
 w_name varchar(10), \
 w_street_1 varchar(20), \
@@ -14,8 +19,8 @@ w_tax varchar(7), \
 w_ytd varchar(15), \
 primary key (w_id) )")
 
-box.sql.execute("drop table if exists district;")
-box.sql.execute("create table district ( \
+box.execute("drop table if exists district;")
+box.execute("create table district ( \
 d_id int not null, \
 d_w_id int not null, \
 d_name varchar(10), \
@@ -30,8 +35,8 @@ d_next_o_id int, \
 primary key (d_w_id, d_id), \
 FOREIGN KEY(d_w_id) REFERENCES warehouse(w_id) );")
 
-box.sql.execute("drop table if exists customer;")
-box.sql.execute("create table customer ( \
+box.execute("drop table if exists customer;")
+box.execute("create table customer ( \
 c_id int not null, \
 c_d_id int not null, \
 c_w_id int not null, \
@@ -56,8 +61,8 @@ c_data text, \
 PRIMARY KEY(c_w_id, c_d_id, c_id), \
 FOREIGN KEY(c_w_id,c_d_id) REFERENCES district(d_w_id,d_id) );")
 
-box.sql.execute("drop table if exists history;")
-box.sql.execute("create table history ( \
+box.execute("drop table if exists history;")
+box.execute("create table history ( \
 _id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \
 h_c_id int, \
 h_c_d_id int, \
@@ -70,8 +75,8 @@ h_data varchar(24), \
 FOREIGN KEY(h_c_w_id,h_c_d_id,h_c_id) REFERENCES customer(c_w_id,c_d_id,c_id), \
 FOREIGN KEY(h_w_id,h_d_id) REFERENCES district(d_w_id,d_id) );")
 
-box.sql.execute("drop table if exists orders;")
-box.sql.execute("create table orders ( \
+box.execute("drop table if exists orders;")
+box.execute("create table orders ( \
 o_id int not null, \
 o_d_id int not null, \
 o_w_id int not null, \
@@ -83,16 +88,16 @@ o_all_local int, \
 PRIMARY KEY(o_w_id, o_d_id, o_id), \
 FOREIGN KEY(o_w_id,o_d_id,o_c_id) REFERENCES customer(c_w_id,c_d_id,c_id) );")
 
-box.sql.execute("drop table if exists new_orders;")
-box.sql.execute("create table new_orders ( \
+box.execute("drop table if exists new_orders;")
+box.execute("create table new_orders ( \
 no_o_id int not null, \
 no_d_id int not null, \
 no_w_id int not null, \
 PRIMARY KEY(no_w_id, no_d_id, no_o_id), \
 FOREIGN KEY(no_w_id,no_d_id,no_o_id) REFERENCES orders(o_w_id,o_d_id,o_id));")
 
-box.sql.execute("drop table if exists item;")
-box.sql.execute("create table item ( \
+box.execute("drop table if exists item;")
+box.execute("create table item ( \
 i_id int not null, \
 i_im_id int, \
 i_name varchar(24), \
@@ -100,8 +105,8 @@ i_price varchar(8), \
 i_data varchar(50), \
 PRIMARY KEY(i_id) );")
 
-box.sql.execute("drop table if exists stock;")
-box.sql.execute("create table stock ( \
+box.execute("drop table if exists stock;")
+box.execute("create table stock ( \
 s_i_id int not null, \
 s_w_id int not null, \
 s_quantity int, \
@@ -123,8 +128,8 @@ PRIMARY KEY(s_w_id, s_i_id), \
 FOREIGN KEY(s_w_id) REFERENCES warehouse(w_id), \
 FOREIGN KEY(s_i_id) REFERENCES item(i_id) );")
 
-box.sql.execute("drop table if exists order_line;")
-box.sql.execute("create table order_line ( \
+box.execute("drop table if exists order_line;")
+box.execute("create table order_line ( \
 ol_o_id int not null, \
 ol_d_id int not null, \
 ol_w_id int not null, \
@@ -139,7 +144,7 @@ PRIMARY KEY(ol_w_id, ol_d_id, ol_o_id, ol_number), \
 FOREIGN KEY(ol_w_id,ol_d_id,ol_o_id) REFERENCES orders(o_w_id,o_d_id,o_id), \
 FOREIGN KEY(ol_supply_w_id,ol_i_id) REFERENCES stock(s_w_id,s_i_id) );")
 
-box.sql.execute("CREATE INDEX idx_customer ON customer (c_w_id,c_d_id,c_last,c_first);")
-box.sql.execute("CREATE INDEX idx_orders ON orders (o_w_id,o_d_id,o_c_id,o_id);")
-box.sql.execute("CREATE INDEX fkey_stock_2 ON stock (s_i_id);")
-box.sql.execute("CREATE INDEX fkey_order_line_2 ON order_line (ol_supply_w_id,ol_i_id);")
+box.execute("CREATE INDEX idx_customer ON customer (c_w_id,c_d_id,c_last,c_first);")
+box.execute("CREATE INDEX idx_orders ON orders (o_w_id,o_d_id,o_c_id,o_id);")
+box.execute("CREATE INDEX fkey_stock_2 ON stock (s_i_id);")
+box.execute("CREATE INDEX fkey_order_line_2 ON order_line (ol_supply_w_id,ol_i_id);")
